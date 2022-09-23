d6fb188b65e1624e84724013a6f3d4be04cbd9bf
kash / Hytus <kash@hytus.net>
2022-09-24 00:53:10 +0900

    feat: Add AudioPatch

--- KatamariFix/Plugin.cs
+++ KatamariFix/Plugin.cs
@@ -1,14 +1,45 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
 
 namespace KatamariFix
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInPlugin("net.hytus.kamatarifix", "KatamariFix", "1.0.0.0")]
     public class Plugin : BaseUnityPlugin
     {
+        internal static ManualLogSource Log;
         private void Awake()
         {
-            // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Log = Logger;
+            var harmony = new Harmony("net.hytus.harmony.kamatarifix");
+            harmony.PatchAll(typeof(AudioPatch));
+            
+        }
+    }
+
+    class AudioPatch
+    {
+        [HarmonyPatch(typeof(SoundController), "Setup")]
+        [HarmonyPostfix]
+        static void SetGameVolumeSilent(SoundController __instance)
+        {
+            Plugin.Log.LogMessage("Patching SoundController::Setup(); Set volume to 1/8");
+            foreach (SoundController.eAudioMixerGroup eAudioMixer in Enum.GetValues(typeof(SoundController.eAudioMixerGroup))) {
+                if (!eAudioMixer.Equals(SoundController.eAudioMixerGroup.MAX))
+                {
+                    Plugin.Log.LogMessage("eAudioMixer: " + eAudioMixer.ToString());
+                    __instance.SetVolume(eAudioMixer, 0.125f);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(UIMoviePlayer), "PlayMove")]
+        [HarmonyPrefix]
+        static void SetMovieVolumeSilent(ref float volume)
+        {
+            Plugin.Log.LogMessage("Patching UIMoviePlayer::PlayMove(); Set volume to 1/8");
+            volume = 0.125f;
         }
     }
 }
