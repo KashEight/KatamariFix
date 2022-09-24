1366b59779feae05dca53f8727b7f4d6fd79a659
kash / Hytus <kash@hytus.net>
2022-09-24 16:04:59 +0900

    chore: split files chore: update project
    Signed-off-by: kash / Hytus <kash@hytus.net>

--- KatamariFix/Plugin.cs
+++ KatamariFix/Plugin.cs
@@ -17,29 +17,4 @@ namespace KatamariFix
             
         }
     }
-
-    class AudioPatch
-    {
-        [HarmonyPatch(typeof(SoundController), "Setup")]
-        [HarmonyPostfix]
-        static void SetGameVolumeSilent(SoundController __instance)
-        {
-            Plugin.Log.LogMessage("Patching SoundController::Setup(); Set volume to 1/8");
-            foreach (SoundController.eAudioMixerGroup eAudioMixer in Enum.GetValues(typeof(SoundController.eAudioMixerGroup))) {
-                if (!eAudioMixer.Equals(SoundController.eAudioMixerGroup.MAX))
-                {
-                    Plugin.Log.LogMessage("eAudioMixer: " + eAudioMixer.ToString());
-                    __instance.SetVolume(eAudioMixer, 0.125f);
-                }
-            }
-        }
-
-        [HarmonyPatch(typeof(UIMoviePlayer), "PlayMove")]
-        [HarmonyPrefix]
-        static void SetMovieVolumeSilent(ref float volume)
-        {
-            Plugin.Log.LogMessage("Patching UIMoviePlayer::PlayMove(); Set volume to 1/8");
-            volume = 0.125f;
-        }
-    }
 }
